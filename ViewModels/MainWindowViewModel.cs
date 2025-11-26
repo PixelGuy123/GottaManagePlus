96152191187bc88ae233939cb250216c57ec6bb8
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2025-11-25 22:22:42 -0300

    Developed even more the UI; Placed Resources dictionary temporarily in MainWindow.axaml for visualizing in Preview properly; WIP: Finish light theme and finish the Mods list (include search bars and a proper label placement).

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,16 @@
-﻿using Avalonia.Media.Imaging;
+﻿using Avalonia;
+using Avalonia.Media.Imaging;
 using GottaManagePlus.Modules.AvaloniaUtils;
 
 namespace GottaManagePlus.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    // Const values
+    const float AllMenus_DefaultRadiusValue = 15f;
+
+    // Real Binding Values
     public Bitmap? GottaSweepLogo { get; } = ImageHelper.LoadAsBitmap("GottaManagePlus.UI.SweepLogo.webp");
+    public CornerRadius AllMenus_CornerRadius_TopRight { get; } = new(0f, AllMenus_DefaultRadiusValue, 0f, 0f);
+    public CornerRadius AllMenus_CornerRadius_All { get; } = new(AllMenus_DefaultRadiusValue);
 }
