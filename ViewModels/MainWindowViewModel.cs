47d7358819452047e07b9219ca1563a535da6bf3
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2025-11-28 13:41:54 -0300

    Whole overhaul to the menu style. It's finally complete. TODO: Make the mod list by learning templates.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,8 +4,4 @@ namespace GottaManagePlus.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    // Const values
-    const float AllMenus_DefaultRadiusValue = 15f;
-    public CornerRadius AllMenus_CornerRadius_LeftScreen { get; } = new(0f, AllMenus_DefaultRadiusValue, AllMenus_DefaultRadiusValue, 0f);
-    public CornerRadius AllMenus_CornerRadius_All { get; } = new(AllMenus_DefaultRadiusValue);
 }
