a8097f65dcf5a06c80a1d98eee7cb34cb7eff8f5
PixelGuy <stickmoderator123@gmail.com>
2025-12-05 13:22:28 -0300

    Finalized list template for mod viewer. WIP: AutoCompleteBox.

--- Models/ModItem.cs
+++ Models/ModItem.cs
@@ -4,11 +4,14 @@ namespace GottaManagePlus.Models;
 
 public class ModItem
 {
-    private const int MaxModNameCutLength = 40;
+    private const int MaxModNameCutLength = 44;
     private string _modName, _cutModName;
 
     public string FullModName { get => _modName; set => UpdateModName(value); }
     public string ModName { get => _cutModName; set => UpdateModName(value); }
+    public string ModVersionString => ModVersion.ToString();
+    
+    public Version ModVersion { get; set; } = new(0, 0, 0); // Default is basically 0
     
     public ModItem()
     {
@@ -24,4 +27,7 @@ public class ModItem
                 string.Concat(_modName.AsSpan(0, MaxModNameCutLength - 3), "...") : // If the name is longer, cut it and insert '...'
                 newName;
     }
+    
+    // Public Methods
+    public void UpdateCutModName() => UpdateModName(_modName); // Just reform the string
 }
